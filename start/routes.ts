fba2ebdc72414b270b2ea50cc7298580a4b19c02
Quentin <quentinmet7@gmail.com>
2024-09-23 15:25:44 +0200

    Fin SDC 2;

--- start/routes.ts
+++ start/routes.ts
@@ -13,20 +13,59 @@ import db from '@adonisjs/lucid/services/db'
 router.get('/lockers', async ({ response }) => {
     const lockers = await db.query().from("lockers").select("*")
     return response.status(200).json(lockers)
-})
+});
 
-router.post('/lockers/add', async ({ request, response }) => {
+router.post('/lockers', async ({ request, response }) => {
     const { locker_name, site, floor, fk_user } = request.all()
+
     await db.table("lockers").insert(
         { 
             locker_name: locker_name,
             site: site,
             floor: floor,
             fk_user: fk_user,
-            created_at: new Date(),
-            updated_at: new Date()
+            created_at: Date.now(),
+            updated_at: Date.now()
         }
     )
 
     return response.status(200).json("Student/Teacher add to a locker sucessfully")
+});
+
+router.put('/lockers/:id', async ({ request, response, params }) => {
+    const id = params.id;
+    const { locker_name, site, floor, fk_user } = request.all()
+
+    const created_at = await db.from("lockers").select("created_at").where("id", id)
+
+    await db.from("lockers").where("id", id).update(
+        {
+            locker_name: locker_name,
+            site: site,
+            floor: floor,
+            fk_user: fk_user,
+            created_at: created_at,
+            updated_at: Date.now()
+        }
+    );
+
+    return response.status(200).json("Lockers modified has been sucessfully")
+});
+
+router.delete('/lockers/:id', async ({ response, params }) => {
+    const id = params.id;
+
+    await db.from("lockers").where("id", id).del()
+
+    return response.status(200).json("Lockers has been deleted sucessfully")
+});
+
+router.post("/lockers/students", async ({ request, response, params, view }) => {
+    const { student_name } = request.all()
+
+    const user_id = await db.from("users").where("username", student_name).select("id")
+
+    await db.from("lockers").update("fk_user", user_id)
+
+    return response.status(200).json("Lockers attributed to a student/teacher")
 })
